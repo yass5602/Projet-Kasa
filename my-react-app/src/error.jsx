d3897fcb2c57a01bb9b651b1b33334ec6ccb9b0e
yass5602 <yasinchk57@gmail.com>
2025-05-05 01:08:19 +0200

    Mise en page de la page error

--- my-react-app/src/error.jsx
+++ my-react-app/src/error.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
- 
+import './scss/error.scss'
+import { Link } from 'react-router-dom'; 
+
  const Error = () => {
-   return <h2>Oups! La page que vous demandez n'existe pas.</h2>;
+  return (
+    <>
+      <div className='error'>
+        <h1>404</h1>
+        <h2>Oups! La page que vous demandez n'existe pas.</h2>
+        <Link to="/" className='retourAccueil'>Retourner sur la page d’accueil</Link>
+      </div>
+    </>
+  )
  };
  
  export default Error;
